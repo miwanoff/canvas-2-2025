2e1a11ea4aa139616b5754c8413057ff4cd90aed
miwanoff <marina.iva.noffa@gmail.com>
2025-03-29 14:58:09 +0200

    Initial commit

---
+++ ball.js
@@ -0,0 +1,8 @@
+const canvas = document.getElementById("canvas");
+const n = 10;
+
+
+context = canvas.getContext("2d");
+let width = canvas.width;
+let height = canvas.height;
+console.log(width, height);
